095367cfe21dc4b075c9174fcb011218c6da47a9
Alexander Chemishki <alexander.chemishki@gmail.com>
2024-03-27 14:07:14 +0300

    Adding base pages

--- car-project/src/app/app.module.ts
+++ car-project/src/app/app.module.ts
@@ -3,14 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AngularFireModule } from '@angular/fire/compat'
+import { environment } from 'src/environments/environments';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { DashboardComponent } from './auth/dashboard/dashboard.component';
+import { FormsModule } from '@angular/forms';
+import { HeaderComponent } from './core/header/header.component';
+import { FooterComponent } from './core/footer/footer.component';
+import { NewCarComponent } from './auth/new-car/new-car.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    DashboardComponent,
+    HeaderComponent,
+    FooterComponent,
+    NewCarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
