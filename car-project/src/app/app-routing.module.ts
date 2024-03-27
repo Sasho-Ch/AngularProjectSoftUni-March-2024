3eb4cc15078a7788078673a7809f97714fbe5974
Alexander Chemishki <alexander.chemishki@gmail.com>
2024-03-27 14:05:21 +0300

    Add routing

--- car-project/src/app/app-routing.module.ts
+++ car-project/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { DashboardComponent } from './auth/dashboard/dashboard.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { HomeComponent } from './home/home.component';
+import { NewCarComponent } from './auth/new-car/new-car.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: 'home', component: HomeComponent},
+  {path: 'dashboard', component: DashboardComponent},
+  {path: 'new-car', component: NewCarComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: 'dashboard', component: DashboardComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
