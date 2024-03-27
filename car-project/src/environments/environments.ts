16f181b4c4921f7acde78fc2995d04d5d1c55503
Alexander Chemishki <alexander.chemishki@gmail.com>
2024-03-27 14:06:42 +0300

    Adding auth service

---
+++ car-project/src/environments/environments.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyAoWLLzM0Z3eIB-OoBbMW9UPgbksQna5dY',
+    authDomain: 'carproject-54d28.firebaseapp.com',
+    projectId: 'carproject-54d28',
+    storageBucket: 'carproject-54d28.appspot.com',
+    messagingSenderId: '703948384760',
+    appId: '1:703948384760:web:ead715943328bd45e9ce0d',
+    measurementId: 'G-H279WD90BB',
+  },
+};
